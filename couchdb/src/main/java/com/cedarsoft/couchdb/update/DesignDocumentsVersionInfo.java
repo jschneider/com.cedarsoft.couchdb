95ec975116353908803813d981f9a5415666288a
Johannes Schneider <js@cedarsoft.com>
2013-05-09 14:11:38 +0200

    Added info object and serializers

--- couchdb/src/main/java/com/cedarsoft/couchdb/update/DesignDocumentsVersionInfo.java
+++ couchdb/src/main/java/com/cedarsoft/couchdb/update/DesignDocumentsVersionInfo.java
@@ -40,4 +40,36 @@ public class DesignDocumentsVersionInfo {
   public String getUpdatedBy() {
     return updatedBy;
   }
+
+  @Override
+  public boolean equals( Object obj ) {
+    if ( this == obj ) {
+      return true;
+    }
+    if ( !( obj instanceof DesignDocumentsVersionInfo ) ) {
+      return false;
+    }
+
+    DesignDocumentsVersionInfo that = ( DesignDocumentsVersionInfo ) obj;
+
+    if ( updatedAt != that.updatedAt ) {
+      return false;
+    }
+    if ( !updatedBy.equals( that.updatedBy ) ) {
+      return false;
+    }
+    if ( !version.equals( that.version ) ) {
+      return false;
+    }
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = version.hashCode();
+    result = 31 * result + ( int ) ( updatedAt ^ updatedAt >>> 32 );
+    result = 31 * result + updatedBy.hashCode();
+    return result;
+  }
 }
---
+++ couchdb/src/test/java/com/cedarsoft/couchdb/update/DesignDocumentsVersionInfoSerializerVersionTest.java
@@ -0,0 +1,32 @@
+
+package com.cedarsoft.couchdb.update;
+
+import com.cedarsoft.serialization.Serializer;
+import com.cedarsoft.serialization.test.utils.AbstractJsonVersionTest2;
+import com.cedarsoft.serialization.test.utils.VersionEntry;
+import com.cedarsoft.version.Version;
+import org.junit.*;
+import org.junit.experimental.theories.*;
+
+import javax.annotation.Nonnull;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class DesignDocumentsVersionInfoSerializerVersionTest extends AbstractJsonVersionTest2<DesignDocumentsVersionInfo> {
+
+  @DataPoint
+  public static final VersionEntry ENTRY1 = DesignDocumentsVersionInfoSerializerVersionTest.create( Version.valueOf( 1, 0, 0 ), DesignDocumentsVersionInfoSerializerVersionTest.class.getResource( "DesignDocumentsVersionInfo_1.0.0_1.json" ) );
+
+  @Nonnull
+  @Override
+  protected Serializer<DesignDocumentsVersionInfo> getSerializer() throws Exception {
+    return new DesignDocumentsVersionInfoSerializer();
+  }
+
+  @Override
+  protected void verifyDeserialized( @Nonnull DesignDocumentsVersionInfo deserialized, @Nonnull Version version ) throws Exception {
+    assertThat( deserialized.getVersion() ).isEqualTo( new Version( 1, 2, 3 ) );
+    assertThat( deserialized.getUpdatedAt() ).isEqualTo( 43L );
+    assertThat( deserialized.getUpdatedBy() ).isEqualTo( "Mark Mustermann@moria2000" );
+  }
+}
